0fb7626bcb3ae17c8db5b596c690730d51041b4f
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-22 11:46:17 +0530

    basic bootsrap.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import Footer from "./pages/Footer";
 import Header from "./pages/Header";
 import MainBody from "./pages/MainBody";
+import "bootstrap/dist/css/bootstrap.css";
 
 // return JSX.
 function App() {
   return (
     <div>
       <Header />
-      <MainBody />
-      <MainBody />
-      <MainBody />
-      <MainBody />
-      <MainBody />
-      <Footer />
+      <BootstrapDemo />
     </div>
   );
 }
 
-
+function BootstrapDemo() {
+  return (
+    <div>
+      <h1 class="bg-primary p-3 text-light">Bootstrap Demo</h1>
+    </div>
+  );
+}
 
 export default App;
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,7 +1,9 @@
+import "bootstrap/dist/css/bootstrap.css";
+
 function Header() {
   return (
     <div>
-      <h1>This Is HEADER!!!!!!!!</h1>
+      <h1 class="bg-info">This Is HEADER!!!!!!!!</h1>
     </div>
   );
 }
