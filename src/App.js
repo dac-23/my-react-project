4ac54e7726e727a8317113c3cbfe0c969a40a2cc
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-22 10:13:14 +0530

    first tag.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-// Note: File Name. It starts with Captial letter
-// Note: File Name and the Function name are same.
-// Note: Make the module public
-
+// JSX => JAVASCRIPT AND XML
+// PREDEFINED TAG: <h1> <p> <div>
+// USER DEFINED TAG: <App></App> or <App />
 function App() {
-  return "Hello World";
+  return <h1>Hello World / ANd more!</h1>;
 }
 
 export default App;
